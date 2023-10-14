8f7b76b977a9363ef513bef3d2b807a4363aad25
data-amit-kumar <data2.amitkumar@gmail.com>
2023-10-14 19:59:18 +0530

    change4

--- app.js
+++ app.js
@@ -28,8 +28,34 @@
 // var secondItem = document.querySelectorAll('.list-group-item');
 // secondItem[1].style.color= 'green';
 
-var oddele= document.querySelectorAll('.list-group-item:nth-child(odd)');
+// var oddele= document.querySelectorAll('.list-group-item:nth-child(odd)');
 
-for(var i=0;i<oddele.length;i++){
-    oddele[i].style.backgroundColor="green";
-}
+// for(var i=0;i<oddele.length;i++){
+//     oddele[i].style.backgroundColor="green";
+// }
+
+// var itemlist= document.querySelector('#items');
+// itemlist.parentNode.style.backgroundColor='grey';
+
+// var itemlist= document.querySelector('#items');
+// itemlist.parentElement.style.backgroundColor='grey';
+
+var newDiv = document.createElement('div');
+
+newDiv.id='hello1';
+newDiv.className='hello';
+
+newDiv.setAttribute('title','Hello Div');
+
+var divText=document.createTextNode("HEllo");
+
+newDiv.appendChild(divText);
+
+console.log(newDiv);
+
+// var container= document.querySelector('header .container');
+// var h1= document.querySelector('header h1');
+
+var container= document.querySelector('div .list-group');
+var ele= document.querySelector('.list-group-item:nth-child(1)');
+container.insertBefore(newDiv,ele);
