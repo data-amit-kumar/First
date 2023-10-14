eb887befcd3bb92f76a5e77a1506dbfa98c221df
data-amit-kumar <data2.amitkumar@gmail.com>
2023-10-14 15:34:53 +0530

    change2

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 //console.dir(document);
 //console.log(document.getElementById('header-title'));
-var headerT=document.getElementById('header-title');
-headerT.textContent="Hello";
-var headerR=document.getElementById('main-header');
-headerR.style.borderBottom= 'solid 3px #000';
+// var headerT=document.getElementById('header-title');
+// headerT.textContent="Hello";
+// var headerR=document.getElementById('main-header');
+// headerR.style.borderBottom= 'solid 3px #000';
+
+var thirdele=document.getElementsByTagName('li')[2];
+thirdele.style.backgroundColor='green';
+var arr=document.getElementsByTagName('li');
+for(var i=0;i<arr.length;i++){
+    arr[i].style.fontWeight='bold';
+}
