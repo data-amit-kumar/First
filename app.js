f63ff90ff76ca897f4cb7b1c47f8184aa393f959
data-amit-kumar <data2.amitkumar@gmail.com>
2023-10-14 17:02:39 +0530

    change3

--- app.js
+++ app.js
@@ -5,9 +5,17 @@
 // var headerR=document.getElementById('main-header');
 // headerR.style.borderBottom= 'solid 3px #000';
 
-var thirdele=document.getElementsByTagName('li')[2];
-thirdele.style.backgroundColor='green';
-var arr=document.getElementsByTagName('li');
-for(var i=0;i<arr.length;i++){
-    arr[i].style.fontWeight='bold';
-}
+// var thirdele=document.getElementsByTagName('li')[2];
+// thirdele.style.backgroundColor='green';
+// var arr=document.getElementsByTagName('li');
+// for(var i=0;i<arr.length;i++){
+//     arr[i].style.fontWeight='bold';
+// }
+
+
+// var it = document.getElementsByClassName('n');
+// it[0].style.color = 'green';
+
+var arr= document.getElementsByTagName('li');
+arr[arr.length-1].style.color='red';
+
