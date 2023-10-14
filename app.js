a375da79dba73c45e64a6960015eb84484a85ebb
data-amit-kumar <data2.amitkumar@gmail.com>
2023-10-14 14:26:32 +0530

    new Changes1

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log(document.getElementById('header-Title'));
+//console.dir(document);
+//console.log(document.getElementById('header-title'));
+var headerT=document.getElementById('header-title');
+headerT.textContent="Hello";
+var headerR=document.getElementById('main-header');
+headerR.style.borderBottom= 'solid 3px #000';
